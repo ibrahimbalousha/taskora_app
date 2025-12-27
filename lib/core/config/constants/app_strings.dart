eccb1eef65f2222a79ed4e29b3db975c2878a772
Ibrahim Balousha <ibrahimbalousha378@gmail.com>
2025-12-27 21:00:07 +0200

    feat: add app strings grouped by screens

---
+++ lib/core/config/constants/app_strings.dart
@@ -0,0 +1,332 @@
+class AppStrings {
+  AppStrings._();
+
+  // ===== common
+  static const String t15S = '15 س';
+  static const String adkhlCodeMrslIlaNumberPhoneKhas =
+      'أدخل الرمز المرسل إلى رقم الجوال الخاص بك.';
+  static const String awlwyaTask = 'أولوية المهمة';
+  static const String ijmalyArbah = 'إجمالي الأرباح';
+  static const String resendSetPasswordPassword = 'إعادة تعيين كلمة المرور!';
+  static const String cancel = 'إلغاء';
+  static const String akhtrPasswordPasswordQwyaLdmanAman =
+      'اختر كلمة مرور قوية لضمان الأمان.';
+  static const String nameProject = 'اسم المشروع';
+  static const String nameTask = 'اسم المهمة';
+  static const String email = 'البريد الإلكتروني';
+  static const String verifyMnNumberJwal = 'التحقق من رقم الجوال!';
+  static const String ryysya = 'الرئيسية';
+  static const String saaatMtwqaa = 'الساعات المتوقعة';
+  static const String projects = 'المشاريع';
+  static const String mlfShkhsy = 'الملف الشخصي';
+  static const String confirmPasswordPasswordNew = 'تأكيد كلمة المرور جديدة *';
+  static const String verifyAn = 'تحقق الان';
+  static const String signOut = 'تسجيل الخروج';
+  static const String signIn = 'تسجيل الدخول';
+  static const String editTask = 'تعديل المهمة';
+  static const String setPasswordPasswordNew = 'تعيين كلمة المرور جديدة *';
+  static const String halaTask = 'حالة المهمة';
+  static const String deleteTask = 'حذف المهمة';
+  static const String save = 'حفظ';
+  static const String numberPhone = 'رقم الجوال *';
+  static const String codeVerifyOtp = 'رمز التحقق OTP *';
+  static const String saaatAml = 'ساعات العمل';
+  static const String adwMndh = 'عضو منذ';
+  static const String qydTnfydh = 'قيد التنفيذ';
+  static const String ktabaTwthyq = 'كتابة التوثيق';
+  static const String password = 'كلمة المرور';
+  static const String lmTstlmRmzAadSendCode = 'لم تستلم الرمز؟ أعد إرسال الرمز';
+  static const String mtwsta = 'متوسطة';
+  static const String mrtfaa = 'مرتفعة';
+  static const String mrhbaName = 'مرحباً {name}';
+  static const String mnkhfda = 'منخفضة';
+  static const String wsfProject = 'وصف المشروع';
+  static const String wsfTask = 'وصف المهمة';
+
+  // ===== onboarding  (Figma: شاشة ترحيبية)
+  static const String onboardingAdfTasksLklMshrwaHddAwlwyathaWttba =
+      'أضف مهام لكل مشروع، حدّد أولوياتها، وتتبع حالة كل مهمة من ToDo حتى Done بسهولة وبساطة.';
+  static const String onboardingAnshyMsharyakHddTfasylhaWtabaTqdmkKhtwa =
+      'أنشئ مشاريعك، حدّد تفاصيلها، وتابع تقدمك خطوة بخطوة في مكان واحد مصمّم خصيصاً للمستقلين.';
+  static const String onboardingIdaraMsharyakBklShwlaWwdwh =
+      'إدارة مشاريعك بكل سهولة ووضوح';
+  static const String onboardingAarfArbahkHqyqyaFwra =
+      'اعرف أرباحك الحقيقية فوراً';
+  static const String onboardingNext = 'التالي';
+  static const String onboardingNzmMhamkWabqAlaMsarValid =
+      'نظّم مهامك وابقَ على المسار الصحيح';
+  static const String onboardingYhsbTtbyqSaaatAmlkTlqayyaWyardLk =
+      'يحسب التطبيق ساعات عملك تلقائياً ويعرض لك أرباحك المكتسبة لكل مهمة أو مشروع، بدقة وشفافية.';
+
+  // ===== signIn  (Figma: Sign In)
+  static const String signInAdkhlPasswordPasswordHna =
+      'أدخل كلمة المرور هنا. ..';
+  static const String signInAwSjlAbr = 'أو سجل عبر';
+  static const String signInIstmrAbrAblApple = 'إستمر عبر أبل / Apple';
+  static const String signInIstmrAbrJwjlGoogle = 'إستمر عبر جوجل / Google';
+  static const String signInTdhkrny = 'تذكرني';
+  static const String signInQmBidkhalDataLtsjylAccountAn =
+      'قم بإدخال البيانات لتسجيل حسابك الان.';
+  static const String signInPasswordPassword = 'كلمة المرور *';
+  static const String signInLdykAccountCreateAccountJdyd =
+      'لديك حساب! إنشاء حساب جديد';
+  static const String signInMrhbaBawdtk = 'مرحباً بعودتك!';
+  static const String signInHlForgotPasswordPassword = 'هل نسيت كلمة المرور!';
+
+  // ===== signInEmail  (Figma: تسجيل الدخول باستخدام الايميل)
+  static const String signInEmailAdkhlBrydkEmail = 'أدخل بريدك الإلكتروني';
+  static const String signInEmailAdkhlPasswordPassword = 'أدخل كلمة المرور';
+  static const String signInEmailEmailAktrwnyNotValid =
+      'البريد الاكتروني غير صحيح';
+  static const String signInEmailSignInLlttbyq = 'تسجيل دخول للتطبيق';
+  static const String signInEmailPasswordPasswordNotShyha =
+      'كلمة المرور غير صحيحة';
+  static const String signInEmailNoYwjdLdykHsabQmBinshaAccount =
+      'لا يوجد لديك حساب؟ قم بإنشاء حساب';
+  static const String signInEmailForgotPassword = 'هل نسيت كلمة المرور؟';
+  static const String signInEmailYsadnyRwytkManaRjaSignInMn =
+      'يسعدني رؤيتك معنا الرجاء تسجيل الدخول من خلال';
+
+  // ===== signUp  (Figma: Sign Up, إنشاء حساب)
+  static const String signUpBinshaHsabFinkTwafqAlaShrwtKhdma =
+      '*بإنشاء حساب، فإنك توافق على شروط الخدمة و سياسة الخصوصية';
+  static const String signUpAdkhlAsmkBalkaml = 'أدخل اسمك بالكامل';
+  static const String signUpAdkhlNameAwlWAkhyrHna =
+      'أدخل الاسم الأول و الاخير هنا. ..';
+  static const String signUpAdkhlBrydykEmail = 'أدخل بريديك الإلكتروني';
+  static const String signUpAdkhlConfirmPasswordPasswordHna =
+      'أدخل تأكيد كلمة المرور هنا. ..';
+  static const String signUpAdkhlSarSaaa = 'أدخل سعر الساعة';
+  static const String signUpAdkhlPasswordPassword = 'أدخل كلمة المرور';
+  static const String signUpAdkhlPasswordPasswordHna =
+      'أدخل كلمة المرور هنا. ..';
+  static const String signUpAnshaAccountAn = 'أنشأ حسابك الان';
+  static const String signUpAnshyAccount = 'أنشئ حسابك.';
+  static const String signUpCreateAccount = 'إنشاء الحساب';
+  static const String signUpCreateAccount2 = 'إنشاء حساب';
+  static const String signUpAdhaYwjdLdykHsabQmBstjylIn =
+      'اذا يوجد لديك حساب؟ قم بستجيل الدخول';
+  static const String signUpUsername = 'اسم المستخدم';
+  static const String signUpNameAwlWAkhyr = 'الاسم الأول و الاخير *';
+  static const String signUpBinshaHsabFinkTwafqAlaShrwtKhdma2 =
+      'بإنشاء حساب، فإنك توافق على شروط الخدمة و سياسة الخصوصية';
+  static const String signUpBalnqrFwqInshyAccountAnFinkTwafq =
+      'بالنقر فوق "إنشئ حسابك الان"، فإنك توافق على الشروط والأحكام وسياسة الخصوصية';
+  static const String signUpConfirmPasswordPassword = 'تأكيد كلمة المرور';
+  static const String signUpConfirmPasswordPassword2 = 'تأكيد كلمة المرور *';
+  static const String signUpSarSaaa = 'سعر الساعة';
+  static const String signUpSytmSaveHdhaSarFyMlfkShkhsy =
+      'سيتم حفظ هذا السعر في ملفك الشخصي واستخدامه كقيمة افتراضية عند إنشاء المشاريع';
+  static const String signUpQmBidkhalDataLanshaAccountAn =
+      'قم بإدخال البيانات لانشاء حسابك الان.';
+  static const String signUpQmBtabaaByanatkKyTtmknMnCreate =
+      'قم بتعبىة بياناتك كي تتمكن من انشاء حساب داخل التطبيق ووتتمع بجميع المميزات بعد انشاء الحساب';
+  static const String signUpPasswordPassword = 'كلمة المرور *';
+  static const String signUpHlLdykAccountBalfalSjlIn =
+      'هل لديك حساب بالفعل! سجّل الدخول';
+
+  // ===== verificationCode  (Figma: كود التفعيل)
+  static const String verificationCodeContinueText = 'المتابعة';
+  static const String verificationCodeTmSendCodeActivationAccountAlaEmail =
+      'تم إرسال رمز تفعيل الحساب على الايميل المرفق أدناه';
+  static const String verificationCodeStnthySlahyaCodeBad =
+      'ستنتهي صلاحية الرمز بعد';
+  static const String verificationCodeCodeActivation = 'كود التفعيل';
+  static const String verificationCodeLmTstlmCodeTfaylResendSend =
+      'لم تستلم رمز التفعيل؟ إعادة الإرسال';
+
+  // ===== forgotPassword  (Figma: نسيت كلمة المرور, هل نسيت كلمة المرور)
+  static const String forgotPasswordAdkhlBrydykEmail = 'أدخل بريديك الإلكتروني';
+  static const String forgotPasswordArsl = 'أرسل';
+  static const String forgotPasswordIhslAlaCodeAn = 'إحصل على الرمز الان';
+  static const String forgotPasswordQmBidkhalNumberPhoneMrtbtBalhsab =
+      'قم بإدخال رقم الجوال المرتبط بالحساب!';
+  static const String forgotPasswordHlForgotPasswordPassword =
+      'هل نسيت كلمة المرور!';
+  static const String forgotPasswordYrjaIdkhalBrydkEmailLirsalCode =
+      'يرجى إدخال بريدك الالكتروني لإرسال الكود';
+
+  // ===== saveNewPassword  (Figma: حفظ كلمة المرور الجديدة)
+  static const String saveNewPasswordAdkhlPasswordPasswordNewHna =
+      'أدخل كلمة المرور جديدة هنا. ..';
+  static const String saveNewPasswordConfirmPasswordPasswordNewHna =
+      'تأكيد كلمة المرور جديدة هنا. ..';
+  static const String saveNewPasswordSavePasswordPasswordNew =
+      'حفظ كلمة المرور الجديدة';
+
+  // ===== passwordResetSuccess  (Figma: مبروك تم إعادة تعيين بنجاح)
+  static const String passwordResetSuccessCongratulationsTmResendSetSuccess =
+      'مبروك, تم إعادة تعيين بنجاح';
+
+  // ===== accountCreatedSuccess  (Figma: مبروك تم إنشاء حسابك بنجاح)
+  static const String
+  accountCreatedSuccessCongratulationsTmCreateAccountSuccess =
+      'مبروك. تم إنشاء حسابك بنجاح!';
+
+  // ===== verificationSuccess  (Figma: تم التحقق بنجاح)
+  static const String verificationSuccessTmVerifySuccess = 'تم التحقق بنجاح!';
+
+  // ===== dashboard  (Figma: لوحة التحكم)
+  static const String dashboardAakhr6Ashhr = 'آخر 6 أشهر';
+  static const String dashboardAjtmaaAmyl = 'اجتماع العميل';
+  static const String dashboardArbahShhrya = 'الأرباح الشهرية';
+  static const String dashboardTasksQadma = 'المهام القادمة';
+  static const String dashboardTasksMtbqya = 'المهام المتبقية';
+  static const String dashboardTasksMktmla = 'المهام المكتملة';
+  static const String dashboardBadYwman = 'بعد يومان';
+  static const String dashboardTsmymWajhaUser = 'تصميم واجهة المستخدم';
+  static const String dashboardTtbyqTjara = 'تطبيق التجارة';
+  static const String dashboardGhda = 'غداً';
+  static const String dashboardLlaml = 'للعمل';
+  static const String dashboardDashboardControl = 'لوحة التحكم';
+  static const String dashboardMrajaaCode = 'مراجعة الكود';
+  static const String dashboardMrhbaNameIlykMlkhsAamalkYwm =
+      'مرحباً {name}، إليك ملخص أعمالك اليوم';
+  static const String dashboardTasksQydTnfydh = 'مهام قيد التنفيذ';
+  static const String dashboardMwqaShrka = 'موقع الشركة';
+  static const String dashboardNzamIdara = 'نظام الإدارة';
+
+  // ===== projects  (Figma: المشاريع)
+  static const String projectsT1018Task = '10/18 مهمة';
+  static const String projectsT1224Task = '12/24 مهمة';
+  static const String projectsT6028saaa = '60/28ساعة';
+  static const String projectsT8045Saaa = '80/45 ساعة';
+  static const String projectsIdaraProjectsWalmhamKhasaBk =
+      'إدارة المشاريع والمهام الخاصة بك';
+  static const String projectsMtjrKbyr = 'المتجر الكبير';
+  static const String projectsTtbyqTjaraIlktrwnya = 'تطبيق التجارة الإلكترونية';
+  static const String projectsShrkaTqnyaHdytha = 'شركة التقنية الحديثة';
+  static const String projectsMjmwaaAamalDwlya = 'مجموعة الأعمال الدولية';
+  static const String projectsMwqaShrkaMwssy = 'موقع الشركة المؤسسي';
+  static const String projectsNzamIdaraMkhzwn = 'نظام إدارة المخزون';
+
+  // ===== projectDetails  (Figma: تفاصيل المشروع)
+  static const String projectDetailsTaskNew = '+ مهمة جديدة';
+  static const String projectDetailsT65Anjaz = '65% الانجاز';
+  static const String projectDetailsIadadQaadaData = 'إعداد قاعدة البيانات';
+  static const String projectDetailsSaaat4580 = 'الساعات 45/80';
+  static const String projectDetailsTasks = 'المهام';
+  static const String projectDetailsMwadRjb = 'الموعد ٣ رجب';
+  static const String projectDetailsTsmymWajhatUser = 'تصميم واجهات المستخدم';
+  static const String projectDetailsTtbyqTjaraAktrwnya =
+      'تطبيق التجارة الاكترونية';
+  static const String projectDetailsTtwyrSfhaSignIn = 'تطوير صفحة تسجيل الدخول';
+  static const String projectDetailsEditProject = 'تعديل المشروع';
+  static const String projectDetailsTkamlWajhatApi = 'تكامل واجهات API';
+  static const String projectDetailsSarProject4000 = 'سعر المشروع \$4000';
+  static const String projectDetailsShrkaTqnyaHdytha = 'شركة التقنية الحديثة';
+  static const String projectDetailsLlaml2 = 'للعمل (2)';
+  static const String projectDetailsMrajaaAman = 'مراجعة الأمان';
+  static const String projectDetailsMktml2 = 'مكتمل (2)';
+
+  // ===== createProject  (Figma: انشاء مشروع)
+  static const String createProjectMmDdYyyy = 'mm/dd/yyyy';
+  static const String createProjectAdkhlNameAmyl = 'أدخل اسم العميل';
+  static const String createProjectAdkhlNameProject = 'أدخل اسم المشروع';
+  static const String createProjectCreateProject = 'إنشاء المشروع';
+  static const String createProjectNameAmylShrkaJha =
+      'اسم العميل / الشركة / الجهة';
+  static const String createProjectDetailsMalyaWalzmnya =
+      'التفاصيل المالية والزمنية';
+  static const String createProjectSarBalsaaa = 'السعر بالساعة (\$)';
+  static const String createProjectMwadNhayy = 'الموعد النهائي';
+  static const String createProjectProjectJdyd = 'مشروع جديد';
+  static const String createProjectMalwmatProject = 'معلومات المشروع';
+  static const String createProjectWsfMkhtsrLlmshrwa = 'وصف مختصر للمشروع';
+
+  // ===== editProject  (Figma: تعديل المشروع)
+  static const String editProjectMwadMqrr = 'الموعد المقرر';
+  static const String editProjectBhdhfkLlmshrwaSytmDeleteProjectMnSfha =
+      'بحذفك للمشروع, سيتم حذف المشروع من صفحة المشاريع وحذف مهامه. .. .. .. .. .. .. .. .. .. .. .. .. .. ..';
+  static const String editProjectTtbyqTjaraAktrwnya =
+      'تطبيق التجارة الاكترونية';
+  static const String editProjectEditProject = 'تعديل المشروع';
+  static const String editProjectTnbyh = 'تنبيه!';
+  static const String editProjectDeleteProject = 'حذف المشروع';
+  static const String editProjectSarProject = 'سعر المشروع';
+  static const String editProjectNsbaAnjaz = 'نسبة الانجاز %';
+
+  // ===== taskDetails  (Figma: تفاصيل المهمة)
+  static const String taskDetailsDetailsTask = 'تفاصيل المهمة';
+  static const String taskDetailsHdhhTaskTttlbTnfydhDqyqWmrajaaShamla =
+      'هذه المهمة تتطلب تنفيذ دقيق ومراجعة شاملة للتأكد من جودة العمل المطلوب.';
+
+  // ===== addTask  (Figma: اضافة مهمة جديدة)
+  static const String addTaskAddTaskNew = 'إضافة مهمة جديدة';
+  static const String addTaskCreateTask = 'إنشاء المهمة';
+  static const String addTaskProjectProjectname = 'المشروع: {projectName}';
+  static const String addTaskTasksNewYtmDbthaTlqayyaAlaQyd =
+      'المهام الجديدة يتم ضبطها تلقائياً على قيد التنفيذ لاحقاً. يمكنك تحديث الحالة بعد الإنشاء من واجهة المشروع أو تفاصيل المهمة.';
+  static const String addTaskWsfMaSytmInjazh = 'وصف ما سيتم إنجازه';
+
+  // ===== editTask  (Figma: تعديل المهمة)
+  static const String editTaskProjectProjectname = 'المشروع: {projectName}';
+
+  // ===== deleteTask  (Figma: حذف المهمة)
+  static const String deleteTaskThdythTask = 'تحديث المهمة';
+  static const String deleteTaskDetailsTask = 'تفاصيل المهمة';
+  static const String deleteTaskDeleteMhma = 'حذف المهمة؟';
+  static const String deleteTaskNo = 'لا';
+  static const String deleteTaskYes = 'نعم';
+  static const String deleteTaskHdhhTaskTttlbTnfydhDqyqWmrajaaShamla =
+      'هذه المهمة تتطلب تنفيذ دقيق ومراجعة شاملة للتأكد من جودة العمل المطلوب.';
+  static const String deleteTaskHlAntMtakdAnkTryd = 'هل أنت متأكد أنك تريد';
+
+  // ===== profile  (Figma: الصفحة الشخصية)
+  static const String profileSarAftradyBalsaaa = 'السعر الافتراضي بالساعة';
+  static const String profileTarykhCreateAccount = 'تاريخ إنشاء الحساب';
+  static const String profileDlylAwlwyat = 'دليل الأولويات';
+  static const String profileMalwmatAccount = 'معلومات الحساب';
+  static const String profileYstkhdmAndCreateProjectJdyd =
+      'يُستخدم عند إنشاء مشروع جديد';
+
+  // ===== signOut  (Figma: تسجيل الخروج)
+  static const String signOutSarAftradyBalsaaa = 'السعر الافتراضي بالساعة';
+  static const String signOutTarykhCreateAccount = 'تاريخ إنشاء الحساب';
+  static const String signOutDlylAwlwyat = 'دليل الأولويات';
+  static const String signOutMalwmatAccount = 'معلومات الحساب';
+  static const String signOutYstkhdmAndCreateProjectJdyd =
+      'يُستخدم عند إنشاء مشروع جديد';
+
+  // ===== noInternet  (Figma: لا يوجد اتصال بالانترنت)
+  static const String noInternetErrorSnackbar = 'Error Snackbar';
+  static const String noInternetInfoSnackbar = 'Info Snackbar';
+  static const String noInternetSnackbarComponents = 'Snackbar Components';
+  static const String noInternetSuccessSnackbar = 'Success Snackbar';
+  static const String noInternetWarningSnackbar = 'Warning Snackbar';
+  static const String noInternetResendMhawla = 'إعادة المحاولة';
+  static const String noInternetAdghtAlaAyZrLardRsalaMnbthqa =
+      'اضغط على أي زر لعرض الرسالة المنبثقة';
+  static const String noInternetTmSaveDataSuccess = 'تم حفظ البيانات بنجاح';
+  static const String noInternetNoYwjdConnectionBalintrnt =
+      'لا يوجد اتصال بالإنترنت';
+  static const String noInternetYrjaVerifyMnShbkaThmResendMhawla =
+      'يرجى التحقق من الشبكة ثم إعادة المحاولة.';
+
+  // ===== noData  (Figma: لا يوجد بيانات)
+  static const String noDataStartAwlKhtwaK = 'ابدأ أول خطوة الك';
+  static const String noDataStzhrMsharyakAwMhamkHnaAndIdaftha =
+      'ستظهر مشاريعك أو مهامك هنا عند إضافتها.';
+  static const String noDataNoTwjdDataHalya = 'لا توجد بيانات حالياً';
+  static const String noDataMaTqlqAwlKhtwaDaymaAshl =
+      'ما تقلق… أول خطوة دايمًا الأسهل!';
+  static const String noDataMrhbaBkFyMjmwaaShrkatYasrHlawa =
+      'مرحباً بك في مجموعة شركات ياسر حلاوة';
+  static const String noDataWlaShyHwnLshWadhInkMa =
+      'ولا شي هون لسه! واضح إنك ما عملت أي إجراء لحد الآن. خلينا نبدأ الرحلة سوا. هاي الصفحة فاضية لأنك ما ضفت ولا عنصر.';
+
+  // ===== unexpectedError  (Figma: حدث خطأ غير متوقع)
+  static const String unexpectedErrorResendMhawla = 'إعادة المحاولة';
+  static const String unexpectedErrorAwda = 'العودة';
+  static const String unexpectedErrorUnexpectedError = 'حدث خطأ غير متوقع';
+  static const String unexpectedErrorLmNtmknMnIkmalAmlyaYrjaMhawla =
+      'لم نتمكن من إكمال العملية، يرجى المحاولة مرة أخرى.';
+
+  // ===== congratulations  (Figma: تهانينا)
+  static const String congratulationsCongratulations = 'تهانينا';
+  static const String congratulationsAccountJahzLlastkhdamSytmThwylkIlaPage =
+      'حسابك جاهز للاستخدام. سيتم تحويلك إلى الصفحة الرئيسية في غضون ثوانٍ قليلة';
+
+  // ===== invalidCode  (Figma: الرمز غير صحيح)
+  static const String invalidCodeCodeNotValid = 'الرمز غير صحيح';
+}
