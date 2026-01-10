05c3bfca798d1388661af71409b4d39798ba78df
Ibrahim Balousha <ibrahimbalousha378@gmail.com>
2026-01-10 23:10:46 +0200

    feat: wire startup flow with bloc and shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:taskora_app/core/config/widgets/buttons/app_icon_elevated_button.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:taskora_app/features/splash_onboarding/data/datasources/splash_onboarding_local_data_source.dart';
+import 'package:taskora_app/features/splash_onboarding/data/repositories/splash_onboarding_repository_impl.dart';
+import 'package:taskora_app/features/splash_onboarding/domain/usecases/check_onboarding_status_usecase.dart';
+import 'package:taskora_app/features/splash_onboarding/domain/usecases/complete_onboarding_usecase.dart';
+import 'package:taskora_app/features/splash_onboarding/presentation/bloc/splash_onboarding_bloc.dart';
+import 'package:taskora_app/features/splash_onboarding/presentation/pages/flutter_splash_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+
+  final localDataSource = SplashOnboardingLocalDataSourceImpl(prefs);
+  final repository = SplashOnboardingRepositoryImpl(localDataSource);
+
+  final checkStatus = CheckOnboardingStatusUseCase(repository);
+  final completeOnboarding = CompleteOnboardingUseCase(repository);
+
+  final splashOnboardingBloc = SplashOnboardingBloc(
+    checkStatus: checkStatus,
+    completeOnboarding: completeOnboarding,
+  );
+
+  runApp(MyApp(bloc: splashOnboardingBloc));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final SplashOnboardingBloc bloc;
+
+  const MyApp({super.key, required this.bloc});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: AppIconElevatedButton(
-            label: 'Greate Task',
-            icon: Icons.access_alarm_sharp,
-            padding: EdgeInsets.symmetric(vertical: 18),
-            onPressed: () {},
-          ),
-        ),
+    return BlocProvider.value(
+      value: bloc,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Taskora',
+        home: FlutterSplashPage(),
       ),
     );
   }
