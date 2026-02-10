da203da3e2db1b457bdf9e19170949d96d42eadf
Ibrahim Balousha <ibrahimbalousha378@gmail.com>
2026-02-10 13:39:44 +0200

    feat(auth): setup auth domain and repository layers

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,22 +1,22 @@
 import 'package:dartz/dartz.dart';
-import 'package:taskora_app/core/error/failuer.dart';
+import 'package:taskora_app/core/error/Failure.dart';
 import 'package:taskora_app/features/auth/domain/entities/auth_token.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failuer, AuthToken>>login({
+  Future<Either<Failure, AuthToken>>login({
     required String identifier,
     required String password,
   });
-  Future<Either<Failuer, Unit>> signUp({
+  Future<Either<Failure, Unit>> signUp({
     required String name,
     required String username,
     required String email,
     required String password,
   });
 
-  Future<Either<Failuer, Unit>> forgotPassword({required String identifier});
+  Future<Either<Failure, Unit>> forgotPassword({required String identifier});
 
-  Future<Either<Failuer, Unit>> resetPassword({
+  Future<Either<Failure, Unit>> resetPassword({
     required String identifier,
     required String newPassword,
     required String resetCode,
