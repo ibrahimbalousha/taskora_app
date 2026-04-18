d9aef9444ab1929412e80969d25fe6a475487ba2
Ibrahim Balousha <ibrahimbalousha378@gmail.com>
2026-04-18 16:59:41 +0300

    refactor(core): update routing and theme constants
    - Refactored router and route names for consistency across app navigation
    - Updated theme constants to improve UI layout and maintain design guidelines

---
+++ lib/core/router/router.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:taskora_app/features/auth/presentation/pages/login_failed.dart';
+import 'package:taskora_app/features/auth/presentation/pages/login_page.dart';
+import 'package:taskora_app/features/auth/presentation/pages/recover_by_email.dart';
+import 'package:taskora_app/features/auth/presentation/pages/signup_page.dart';
+import 'package:taskora_app/features/auth/presentation/pages/verification_code_page.dart';
+import 'package:taskora_app/features/splash_onboarding/presentation/pages/flutter_splash_page.dart';
+import 'package:taskora_app/features/splash_onboarding/presentation/pages/home_page.dart';
+import 'package:taskora_app/features/splash_onboarding/presentation/pages/onboarding_page.dart';
+import 'routers_name.dart';
+
+class AppRouter {
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      case RoutesName.onboardingPage:
+        return MaterialPageRoute(builder: (_) => const OnboardingPage());
+        
+ case RoutesName.flutterSplashPage:
+        return MaterialPageRoute(builder: (_) => const FlutterSplashPage());
+        
+      case RoutesName.signup:
+        return MaterialPageRoute(builder: (_) => const SignUpPage());
+      case RoutesName.home:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      case RoutesName.loginFailed:
+        return MaterialPageRoute(builder: (_) => const LoginFailed());
+      case RoutesName.recoverByEmail:
+        return MaterialPageRoute(builder: (_) => const RecoverByEmail());
+      case RoutesName.verificationCode:
+        return MaterialPageRoute(builder: (_) => const VerificationCodePage());
+
+      default:
+        return null;
+    }
+  }
+}
---
+++ lib/core/router/routers_name.dart
@@ -0,0 +1,10 @@
+class RoutesName {
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
+  static const String loginFailed = '/loginFailed';
+  static const String recoverByEmail = '/recoverByEmail';
+  static const String verificationCode = '/verificationCode';
+  static const String onboardingPage = '/onboardingPage';
+  static const String flutterSplashPage = '/flutterSplashPage';
+}
