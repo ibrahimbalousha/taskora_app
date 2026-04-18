0e561c5903660f5b9794829fb8285b08bb07452f
Ibrahim Balousha <ibrahimbalousha378@gmail.com>
2026-04-18 16:54:57 +0300

    feat(auth/presentation): update login and signup pages to use AuthBloc and new fields

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,139 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:taskora_app/core/config/constants/app_sizes.dart';
 import 'package:taskora_app/core/config/constants/app_strings.dart';
-import 'package:taskora_app/core/config/constants/color_manager.dart';
 import 'package:taskora_app/core/config/widgets/app_bars/custom_app_bar.dart';
 import 'package:taskora_app/core/config/widgets/buttons/app_elevated_button.dart';
 import 'package:taskora_app/core/config/widgets/inputs/app_text_field.dart';
 import 'package:taskora_app/core/config/widgets/text/app_rich_text.dart';
 import 'package:taskora_app/core/config/widgets/text/blue_main_text.dart';
-import 'package:taskora_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:taskora_app/features/auth/domain/usecases/forgot_password_usecase.dart';
-import 'package:taskora_app/features/auth/domain/usecases/reset_password_usecase.dart';
-import 'package:taskora_app/features/auth/domain/usecases/verify_reset_code_usecase.dart';
+import 'package:taskora_app/core/router/routers_name.dart';
 import 'package:taskora_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:taskora_app/features/auth/presentation/bloc/auth_event.dart';
 import 'package:taskora_app/features/auth/presentation/bloc/auth_state.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-   return BlocProvider<AuthBloc>(
-  create: (_) => AuthBloc(
-    forgotPasswordUseCase: ForgotPasswordUseCase(AuthRepositoryImpl()),
-    verifyResetCodeUseCase: VerifyResetCodeUseCase(AuthRepositoryImpl()),
-    resetPasswordUseCase: ResetPasswordUseCase(AuthRepositoryImpl()),
-  ),
-  child: const _LoginView(),
-);
-
-  }
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginView extends StatefulWidget {
-  const _LoginView();
-
-  @override
-  State<_LoginView> createState() => _LoginViewState();
-}
-
-class _LoginViewState extends State<_LoginView> {
+class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   bool rememberMe = false;
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocListener<AuthBloc, AuthState>(
       listener: (context, state) {
-        if (state is AuthLoading) {
-          // show loader if needed
-        } else if (state is LoginSuccess) {
-          // navigate to home لاحقًا
+        if (state is LoginSuccess) {
+          Navigator.pushReplacementNamed(context, RoutesName.home);
         } else if (state is AuthError) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text(state.message)));
+          Navigator.pushNamed(context, RoutesName.loginFailed);
         }
       },
       child: Scaffold(
-        appBar: CustomAppBar(
-          title: AppStringsAuth.logIn,
-          leading: const Icon(Icons.arrow_back_ios_new),
-        ),
+        appBar: CustomAppBar(title: AppStringsAuth.logIn),
         body: Padding(
-          padding: AppPadding.horizontalPagePadding,
+          padding: AppPadding.horizontalPagePaddingAndTop,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: AppPadding.topPagePadding,
-                child: BlueMainText(text: AppStringsAuth.welcomeBack),
-              ),
+              BlueMainText(text: AppStringsAuth.welcomeBack),
               Text(AppStringsAuth.pleaseLoginOrSignUpToContinueOurOpp),
 
-              /// Email
               Padding(
-                padding: AppPadding.topTextFieldPadding,
+                padding: AppPadding.topWidgetPadding,
                 child: AppTextField(
                   controller: _emailController,
                   label: 'E-mail',
-                  hint: 'example@gmail.com',
+                  hint:  AppStringsAuth.hintEmailExample,
                   keyboardType: TextInputType.emailAddress,
                   prefixIcon: const Icon(Icons.email_outlined),
                 ),
               ),
 
-              /// Password
               Padding(
-                padding: AppPadding.topTextFieldPadding,
+                padding: AppPadding.topWidgetPadding,
                 child: AppTextField(
                   controller: _passwordController,
                   label: 'Password',
                   hint: AppStringsAuth.passwordExample,
                   obscureText: true,
                   prefixIcon: const Icon(Icons.lock_outline),
-                  suffixIcon: const Icon(Icons.visibility_off),
                 ),
               ),
 
-              /// Remember me
               Row(
                 children: [
                   Checkbox(
                     value: rememberMe,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                    fillColor: WidgetStateProperty.all(Colors.white),
-                    side: const BorderSide(
-                      color: ColorManager.primary,
-                      width: 1.5,
-                    ),
-                    checkColor: ColorManager.primary,
-                    visualDensity: VisualDensity.compact,
                     onChanged: (value) {
                       setState(() {
                         rememberMe = value ?? false;
                       });
                     },
                   ),
-                  const Text(
-                    AppStringsCommon.rememberMe,
-                    style: TextStyle(
-                      color: ColorManager.textPrimary,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
+                  const Text(AppStringsCommon.rememberMe),
                 ],
               ),
 
-              /// Login button
               Padding(
                 padding: AppPadding.elevatedButtonPadding,
                 child: BlocBuilder<AuthBloc, AuthState>(
@@ -141,11 +96,15 @@ class _LoginViewState extends State<_LoginView> {
                     return AppElevatedButton(
                       label: AppStringsAuth.logIn,
                       isLoading: state is AuthLoading,
-                      onPressed: () {
+                      onPressed: () async {
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+
                         context.read<AuthBloc>().add(
                           LoginEvent(
-                            identifier: _emailController.text.trim(),
+                            email: _emailController.text.trim(),
                             password: _passwordController.text.trim(),
+                            username: prefs.getString('username')!,
                           ),
                         );
                       },
@@ -154,14 +113,13 @@ class _LoginViewState extends State<_LoginView> {
                 ),
               ),
 
-              /// Navigate to signup
               Align(
                 alignment: Alignment.center,
                 child: AppRichText(
                   text: AppStringsAuth.dontHaveAnAccount,
                   actionText: AppStringsAuth.createAAccount,
                   onTap: () {
-                    // TODO: navigate to signup
+                    Navigator.pushNamed(context, RoutesName.signup);
                   },
                 ),
               ),
--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,65 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:taskora_app/core/config/constants/app_sizes.dart';
 import 'package:taskora_app/core/config/constants/app_strings.dart';
 import 'package:taskora_app/core/config/widgets/app_bars/custom_app_bar.dart';
 import 'package:taskora_app/core/config/widgets/buttons/app_elevated_button.dart';
 import 'package:taskora_app/core/config/widgets/inputs/app_text_field.dart';
 import 'package:taskora_app/core/config/widgets/text/app_rich_text.dart';
+import 'package:taskora_app/core/router/routers_name.dart';
+import 'package:taskora_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:taskora_app/features/auth/presentation/bloc/auth_event.dart';
+import 'package:taskora_app/features/auth/presentation/bloc/auth_state.dart';
 
-class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final nameController = TextEditingController();
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final watchCostController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    usernameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    watchCostController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: AppStringsAuth.createAAccount,
-        leading: Icon(Icons.arrow_back_ios_new),
-      ),
-      body: Padding(
-        padding: AppPadding.horizontalPagePadding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: AppPadding.topPagePadding,
-              child: Text(
-                AppStringsAuth.createYourAccountAndEnjoyTheBestServices,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is SignUpSuccess) {
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            RoutesName.login,
+            (route) => false,
+          );
+        }
+
+        if (state is AuthError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.message)));
+        }
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: AppStringsAuth.createAAccount,
+          leading: IconButton(
+            onPressed: () {
+              if (Navigator.canPop(context)) {
+                Navigator.pop(context);
+              }
+            },
+            icon: Icon(Icons.arrow_back_ios_new),
+          ),
+        ),
+        body: Padding(
+          padding: AppPadding.horizontalPagePaddingAndTop,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(AppStringsAuth.createYourAccountAndEnjoyTheBestServices),
+              AppTextField(
+                label: 'Name',
+                hint: AppStringsAuth.enterYourName,
+                controller: nameController,
               ),
-            ),
-            AppTextField(label: 'Name', hint: AppStringsAuth.enterYourName),
-            AppTextField(
-              label: 'E-mail',
-              hint: AppStringsAuth.hintEmailExample,
-              prefixIcon: const Icon(Icons.email_outlined),
-            ),
-            AppTextField(
-              label: 'Password',
-              hint: AppStringsAuth.passwordExample,
-              prefixIcon: const Icon(Icons.lock_outline),
-              suffixIcon: Icon(Icons.visibility_off),
-            ),
-            AppTextField(
-              label: 'Hourly Rate ',
-              hint: '\$50',
-              suffixIcon: Icon(Icons.monetization_on_outlined),
-            ),
-            Padding(
-              padding: AppPadding.elevatedButtonPadding,
-              child: AppElevatedButton(
-                label: AppStringsAuth.signUp,
-                onPressed: () {},
+              AppTextField(
+                label: 'UserName',
+                hint: AppStringsAuth.enterYourUserName,
+                controller: usernameController,
               ),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: AppRichText(
-                text: AppStringsAuth.doYouHaveAnAccount,
-                actionText: AppStringsAuth.logIn,
-                onTap: () {},
+
+              AppTextField(
+                label: 'E-mail',
+                hint: AppStringsAuth.hintEmailExample,
+                prefixIcon: const Icon(Icons.email_outlined),
+                controller: emailController,
               ),
-            ),
-          ],
+              AppTextField(
+                label: 'Password',
+                hint: AppStringsAuth.passwordExample,
+                prefixIcon: const Icon(Icons.lock_outline),
+                suffixIcon: Icon(Icons.visibility_off),
+                controller: passwordController,
+              ),
+              AppTextField(
+                label: 'Hourly Rate ',
+                hint: '\$50',
+                suffixIcon: Icon(Icons.monetization_on_outlined),
+                controller: watchCostController,
+              ),
+              Padding(
+                padding: AppPadding.elevatedButtonPadding,
+                child: BlocBuilder<AuthBloc, AuthState>(
+                  builder: (context, state) {
+                    if (state is AuthLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    return AppElevatedButton(
+                      label: AppStringsAuth.signUp,
+                      onPressed: () async {
+                        context.read<AuthBloc>().add(
+                          SignUpEvent(
+                            name: nameController.text.trim(),
+                            username: usernameController.text.trim(),
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim(),
+                            watchCost: watchCostController.text.trim(),
+                          ),
+                        );
+                        SharedPreferences prefs =
+                            await SharedPreferences.getInstance();
+                        prefs.setString(
+                          'username',
+                          usernameController.text.trim(),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: AppRichText(
+                  text: AppStringsAuth.doYouHaveAnAccount,
+                  actionText: AppStringsAuth.logIn,
+                  onTap: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      RoutesName.login,
+                      (route) => false,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
